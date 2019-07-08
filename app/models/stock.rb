1f2ca0b92d5a1fd9f7d478fd3d894c02a382867a
Bhanu9650 <bhanugupta.im@gmail.com>
2019-07-08 12:00:47 +0530

    Add search stock ajax and errors

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,25 +1,20 @@
 class Stock < ApplicationRecord
-	def self.find_by_ticker(ticker_symbol)
-		where(ticker: ticker_symbol).first
-
-	end
-
-
+	
 	def self.new_from_lookup(ticker_symbol)
-		looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-		return nil unless looked_up_stock.name
-
-		new_stock = new(ticker: looked_up_stock, name: looked_up_stock.name)
-		new_stock.last_price = new_stock.price
-		new_stock
+		begin
+			looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+			#price = strip_commas(looked_up_stock.l) this will done below rails 5 because of decimal
+			new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+		rescue Exception => e
+			return nil
+		end
 	end
 
-	def price
-	    closing_price = StockQuote::Stock.quote(ticker).close
-	    return "#{closing_price} (closing)" if closing_price
-
-	    opening_price = StockQuote::Stock.quote(ticker).open
-	    return "#{opening_price (opening)}" if opening_price
-	    "Unavailable"
-	end
+	#def price
+	 #   closing_price = StockQuote::Stock.quote(ticker).close
+	  #  return "#{closing_price} (closing)" if closing_price
+	   # opening_price = StockQuote::Stock.quote(ticker).open
+	    #return "#{opening_price (opening)}" if opening_price
+	    #"Unavailable"
+	#end
 end
